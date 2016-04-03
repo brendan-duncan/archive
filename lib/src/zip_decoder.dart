56ddc18c8d4ff38e101d02016f47a327549d043b
Dominik Henneke <dhenneke@gmail.com>
2016-04-03 23:14:25 +0200

    Set the isFile flag correct and extract the unix permissions from zip archives (only works for unix zip files).

--- lib/src/zip_decoder.dart
+++ lib/src/zip_decoder.dart
@@ -17,6 +17,12 @@ class ZipDecoder {
     for (ZipFileHeader zfh in directory.fileHeaders) {
       ZipFile zf = zfh.file;
 
+      // The attributes are stored in base 8
+      final unixAttributes = zfh.externalFileAttributes >> 16;
+      final unixPermissions = unixAttributes & 0x1FF;
+      final isDirectory = unixAttributes & 0x7000 == 0x4000;
+      final isFile = unixAttributes & 0x3F000 == 0x8000;
+
       if (verify) {
         int computedCrc = getCrc32(zf.content);
         if (computedCrc != zf.crc32) {
@@ -26,7 +32,13 @@ class ZipDecoder {
 
       var content = zf._content != null ? zf._content : zf._rawContent;
       ArchiveFile file = new ArchiveFile(zf.filename, zf.uncompressedSize,
-                                         content, zf.compressionMethod);
+          content, zf.compressionMethod)
+        ..unixPermissions = unixPermissions;
+
+      if (isFile || isDirectory) {
+        file.isFile = isFile;
+      }
+
       file.crc32 = zf.crc32;
 
       archive.addFile(file);
