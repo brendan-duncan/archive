e2b09b92ee7601748ebc62d72b78da005c13b67f
brendan_duncan <brendanduncan@gmail.com>
2020-01-02 21:40:54 -0800

    export extractArchiveToDisk function

--- lib/archive_io.dart
+++ lib/archive_io.dart
@@ -3,6 +3,7 @@ library archive_io;
 export 'archive.dart';
 
 export 'src/io/create_archive_from_directory.dart';
+export 'src/io/extract_archive_to_disk.dart';
 export 'src/io/input_file_stream.dart';
 export 'src/io/output_file_stream.dart';
 export 'src/io/tar_file_encoder.dart';
