4296b435c0c82dd11a2ab64908a15a1b720ee753
paco <picc2711@usherbrooke.ca>
2020-07-08 15:24:49 -0400

    Add zlib raw encoding/decoding

--- lib/src/zlib/zlib_decoder_base.dart
+++ lib/src/zlib/zlib_decoder_base.dart
@@ -2,11 +2,13 @@ import '../util/input_stream.dart';
 
 /// Decompress data with the zlib format decoder.
 class ZLibDecoderBase {
-  List<int> decodeBytes(List<int> data, {bool verify = false}) {
+  List<int> decodeBytes(List<int> data,
+      {bool verify = false, bool raw = false}) {
     return null;
   }
 
-  List<int> decodeBuffer(InputStream input, {bool verify = false}) {
+  List<int> decodeBuffer(InputStream input,
+      {bool verify = false, bool raw = false}) {
     return null;
   }
 }
