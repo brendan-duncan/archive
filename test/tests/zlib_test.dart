db0df834b9e0be1762e565a65735f9ac59dab762
paco <picc2711@usherbrooke.ca>
2020-07-08 15:51:46 -0400

    Add test for zlib raw encode/decode

--- test/tests/zlib_test.dart
+++ test/tests/zlib_test.dart
@@ -9,11 +9,18 @@ void main() {
 
   test('encode/decode', () {
     final compressed = ZLibEncoder().encode(buffer);
-    final decompressed =
-        ZLibDecoder().decodeBytes(compressed, verify: true);
+    final decompressed = ZLibDecoder().decodeBytes(compressed, verify: true);
     expect(decompressed.length, equals(buffer.length));
     for (var i = 0; i < buffer.length; ++i) {
       expect(decompressed[i], equals(buffer[i]));
+    }
+  });
+  test('raw encode/raw decode', () {
+    final compressedRaw = ZLibEncoder().encode(buffer, raw: true);
+    final decompressedRaw = ZLibDecoder().decodeBytes(compressedRaw, raw: true);
+    expect(decompressedRaw.length, equals(buffer.length));
+    for (var i = 0; i < buffer.length; ++i) {
+      expect(decompressedRaw[i], equals(buffer[i]));
     }
   });
 }
