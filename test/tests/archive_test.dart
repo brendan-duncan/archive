a764be10cbeed745a70e0c72c4876e34b7217ab9
Brendan Duncan <brendan.duncan@unity3d.com>
2022-03-24 21:51:31 -0700

    Add non-ascii ArchiveFile name to test

--- test/tests/archive_test.dart
+++ test/tests/archive_test.dart
@@ -13,14 +13,18 @@ void main() {
 
       archive.addFile(ArchiveFile("b", 1, Uint8List.fromList([3])));
 
-      expect(archive.length, 3);
+      archive.addFile(ArchiveFile("陳大文_1_test.png", 1, [4]));
+
+      expect(archive.length, 4);
       expect(archive[0].name, "a");
       expect(archive[1].name, "b");
       expect(archive[2].name, "c");
+      expect(archive[3].name, "陳大文_1_test.png");
 
       expect(archive[0].content[0], 0);
       expect(archive[1].content[0], 3);
       expect(archive[2].content[0], 2);
+      expect(archive[3].content[0], 4);
     });
 
     test('clear', () {
