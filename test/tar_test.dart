ae9f180588de05f793188e45224d2f5549f1f5a6
Brendan Duncan <brendanduncan@gmail.com>
2024-04-21 21:40:32 -0600

    Update tar_test.dart

--- test/tar_test.dart
+++ test/tar_test.dart
@@ -149,179 +149,184 @@ var tarTests = [
 ];
 
 void main() {
-  test('tar invalid archive', () {
-    try {
-      TarDecoder().decodeBytes(Uint8List.fromList([1, 2, 3]));
-      assert(false);
-    } catch (e) {
-      // pass
-    }
-  });
-
-  test('tar file', () {
-    final tar = TarEncoder()
-        .encode(Archive()..add(ArchiveFile.list('file.txt', [100])));
-    File(p.join(testOutputPath, 'tar_encoded.tar'))
-      ..createSync(recursive: true)
-      ..writeAsBytesSync(tar);
-  });
-
-  test('tar file with symlink', () {
-    ArchiveFile symlink = ArchiveFile.symlink('file.txt', 'file2.txt');
-    final tar = TarEncoder().encode(Archive()..add(symlink));
-    File(p.join(testOutputPath, 'tar_encoded.tar'))
-      ..createSync(recursive: true)
-      ..writeAsBytesSync(tar);
-    final archive = TarDecoder().decodeBytes(tar);
-    expect(archive[0].isSymbolicLink, true);
-  });
-
-  test('long file name', () {
-    final file = File('test/_data/tar/x.tar');
-    final bytes = file.readAsBytesSync();
-    final archive = TarDecoder().decodeBytes(bytes, verify: true);
-
-    expect(archive.length, equals(1));
-    var x = '';
-    for (var i = 0; i < 150; ++i) {
-      x += 'x';
-    }
-    x += '.txt';
-    expect(archive[0].name, equals(x));
-  });
-
-  test('long file name not null terminated', () async {
-    final bytes = await http.readBytes(Uri.parse(
-        'https://pub.dev/packages/firebase_messaging/versions/10.0.8.tar.gz'));
-    final tarBytes = GZipDecoder().decodeBytes(bytes, verify: true);
-    final archive = TarDecoder().decodeBytes(tarBytes, verify: true);
-
-    expect(archive.length, equals(129));
-    expect(
-        archive[13].name,
-        equals(
-            'android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingBackgroundExecutor.java'));
-  });
-
-  test('symlink', () {
-    var file = File('test/_data/tar/symlink_tar.tar');
-    final bytes = file.readAsBytesSync();
-    final archive = TarDecoder().decodeBytes(bytes, verify: true);
-    expect(archive.length, equals(4));
-    expect(archive[1].isSymbolicLink, equals(true));
-    expect(archive[1].symbolicLink, equals('b/b.txt'));
-  });
-
-  test('decode test2.tar', () {
-    final file = File('test/_data/test2.tar');
-    final bytes = file.readAsBytesSync();
-    final archive = TarDecoder().decodeBytes(bytes, verify: true);
-
-    final expectedFiles = <File>[];
-    listDir(expectedFiles, Directory('test/_data/test2'));
-
-    expect(archive.length, equals(4));
-  });
-
-  test('decode test2.tar.gz', () {
-    final file = File('test/_data/test2.tar.gz');
-    var bytes = file.readAsBytesSync();
-
-    bytes = GZipDecoder().decodeBytes(bytes, verify: true);
-    final archive = TarDecoder().decodeBytes(bytes, verify: true);
-
-    final expectedFiles = <File>[];
-    listDir(expectedFiles, Directory('test/_data/test2'));
-
-    expect(archive.length, equals(4));
-  });
-
-  test('decode/encode', () {
-    /*final aBytes = aTxt.codeUnits;
-
-    var b = File('test/_data/cat.jpg');
-    List<int> bBytes = b.readAsBytesSync();
-
-    var file = File('test/_data/test.tar');
-    final bytes = file.readAsBytesSync();
-
-    final archive = tar.decodeBytes(bytes, verify: true);
-    expect(archive.length, equals(2));
-
-    var tFile = archive.fileName(0);
-    expect(tFile, equals('a.txt'));
-    var tBytes = archive.fileData(0);
-    compareBytes(tBytes, aBytes);
-
-    tFile = archive.fileName(1);
-    expect(tFile, equals('cat.jpg'));
-    tBytes = archive.fileData(1);
-    compareBytes(tBytes, bBytes);
-
-    final encoded = tarEncoder.encode(archive);
-    final out = File(p.join(testOutputPath, 'test.tar'));
-    out.createSync(recursive: true);
-    out.writeAsBytesSync(encoded);
-
-    // Test round-trip
-    final archive2 = tar.decodeBytes(encoded, verify: true);
-    expect(archive2.length, equals(2));
-
-    tFile = archive2.fileName(0);
-    expect(tFile, equals('a.txt'));
-    tBytes = archive2.fileData(0);
-    compareBytes(tBytes, aBytes);
-
-    tFile = archive2.fileName(1);
-    expect(tFile, equals('cat.jpg'));
-    tBytes = archive2.fileData(1);
-    compareBytes(tBytes, bBytes);*/
-  });
-
-  for (Map<String, dynamic> t in tarTests) {
-    test('untar ${t['file']}', () {
-      var file = File(p.join('test', t['file'] as String));
-      var bytes = file.readAsBytesSync();
-
-      final tar = TarDecoder();
-      /*Archive archive =*/ tar.decodeBytes(bytes, verify: true);
-      expect(tar.files.length, equals(t['headers'].length));
-
-      for (var i = 0; i < tar.files.length; ++i) {
-        final file = tar.files[i];
-        var hdr = t['headers'][i] as Map<String, dynamic>;
-
-        if (hdr.containsKey('Name')) {
-          expect(file.filename, equals(hdr['Name']));
-        }
-        if (hdr.containsKey('Mode')) {
-          expect(file.mode, equals(hdr['Mode']));
-        }
-        if (hdr.containsKey('Uid')) {
-          expect(file.ownerId, equals(hdr['Uid']));
-        }
-        if (hdr.containsKey('Gid')) {
-          expect(file.groupId, equals(hdr['Gid']));
-        }
-        if (hdr.containsKey('Size')) {
-          expect(file.fileSize, equals(hdr['Size']));
-        }
-        if (hdr.containsKey('Linkname')) {
-          expect(file.nameOfLinkedFile, equals(hdr['Linkname']));
-        }
-        if (hdr.containsKey('ModTime')) {
-          expect(file.lastModTime, equals(hdr['ModTime']));
-        }
-        if (hdr.containsKey('Typeflag')) {
-          expect(file.typeFlag, equals(hdr['Typeflag']));
-        }
-        if (hdr.containsKey('Uname')) {
-          expect(file.ownerUserName, equals(hdr['Uname']));
-        }
-        if (hdr.containsKey('Gname')) {
-          expect(file.ownerGroupName, equals(hdr['Gname']));
-        }
+  group('tar', () {
+    test('invalid archive', () {
+      try {
+        TarDecoder().decodeBytes(Uint8List.fromList([1, 2, 3]));
+        assert(false);
+      } catch (e) {
+        // pass
       }
     });
-  }
+
+    test('file', () {
+      final tar = TarEncoder()
+          .encode(Archive()
+        ..add(ArchiveFile.list('file.txt', [100])));
+      File(p.join(testOutputPath, 'tar_encoded.tar'))
+        ..createSync(recursive: true)
+        ..writeAsBytesSync(tar);
+    });
+
+    test('file with symlink', () {
+      ArchiveFile symlink = ArchiveFile.symlink('file.txt', 'file2.txt');
+      final tar = TarEncoder().encode(Archive()
+        ..add(symlink));
+      File(p.join(testOutputPath, 'tar_encoded.tar'))
+        ..createSync(recursive: true)
+        ..writeAsBytesSync(tar);
+      final archive = TarDecoder().decodeBytes(tar);
+      expect(archive[0].isSymbolicLink, true);
+    });
+
+    test('long file name', () {
+      final file = File('test/_data/tar/x.tar');
+      final bytes = file.readAsBytesSync();
+      final archive = TarDecoder().decodeBytes(bytes, verify: true);
+
+      expect(archive.length, equals(1));
+      var x = '';
+      for (var i = 0; i < 150; ++i) {
+        x += 'x';
+      }
+      x += '.txt';
+      expect(archive[0].name, equals(x));
+    });
+
+    test('long file name not null terminated', () async {
+      final bytes = await http.readBytes(Uri.parse(
+          'https://pub.dev/packages/firebase_messaging/versions/10.0.8.tar.gz'));
+      final tarBytes = GZipDecoder().decodeBytes(bytes, verify: true);
+      final archive = TarDecoder().decodeBytes(tarBytes, verify: true);
+
+      expect(archive.length, equals(129));
+      expect(
+          archive[13].name,
+          equals(
+              'android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingBackgroundExecutor.java'));
+    });
+
+    test('symlink', () {
+      var file = File('test/_data/tar/symlink_tar.tar');
+      final bytes = file.readAsBytesSync();
+      final archive = TarDecoder().decodeBytes(bytes, verify: true);
+      expect(archive.length, equals(4));
+      expect(archive[1].isSymbolicLink, equals(true));
+      expect(archive[1].symbolicLink, equals('b/b.txt'));
+    });
+
+    test('decode test2.tar', () {
+      final file = File('test/_data/test2.tar');
+      final bytes = file.readAsBytesSync();
+      final archive = TarDecoder().decodeBytes(bytes, verify: true);
+
+      final expectedFiles = <File>[];
+      listDir(expectedFiles, Directory('test/_data/test2'));
+
+      expect(archive.length, equals(4));
+    });
+
+    test('decode test2.tar.gz', () {
+      final file = File('test/_data/test2.tar.gz');
+      var bytes = file.readAsBytesSync();
+
+      bytes = GZipDecoder().decodeBytes(bytes, verify: true);
+      final archive = TarDecoder().decodeBytes(bytes, verify: true);
+
+      final expectedFiles = <File>[];
+      listDir(expectedFiles, Directory('test/_data/test2'));
+
+      expect(archive.length, equals(4));
+    });
+
+    test('decode/encode', () {
+      /*final aBytes = aTxt.codeUnits;
+
+      var b = File('test/_data/cat.jpg');
+      List<int> bBytes = b.readAsBytesSync();
+
+      var file = File('test/_data/test.tar');
+      final bytes = file.readAsBytesSync();
+
+      final archive = tar.decodeBytes(bytes, verify: true);
+      expect(archive.length, equals(2));
+
+      var tFile = archive.fileName(0);
+      expect(tFile, equals('a.txt'));
+      var tBytes = archive.fileData(0);
+      compareBytes(tBytes, aBytes);
+
+      tFile = archive.fileName(1);
+      expect(tFile, equals('cat.jpg'));
+      tBytes = archive.fileData(1);
+      compareBytes(tBytes, bBytes);
+
+      final encoded = tarEncoder.encode(archive);
+      final out = File(p.join(testOutputPath, 'test.tar'));
+      out.createSync(recursive: true);
+      out.writeAsBytesSync(encoded);
+
+      // Test round-trip
+      final archive2 = tar.decodeBytes(encoded, verify: true);
+      expect(archive2.length, equals(2));
+
+      tFile = archive2.fileName(0);
+      expect(tFile, equals('a.txt'));
+      tBytes = archive2.fileData(0);
+      compareBytes(tBytes, aBytes);
+
+      tFile = archive2.fileName(1);
+      expect(tFile, equals('cat.jpg'));
+      tBytes = archive2.fileData(1);
+      compareBytes(tBytes, bBytes);*/
+    });
+
+    for (Map<String, dynamic> t in tarTests) {
+      test('untar ${t['file']}', () {
+        var file = File(p.join('test', t['file'] as String));
+        var bytes = file.readAsBytesSync();
+
+        final tar = TarDecoder();
+        /*Archive archive =*/
+        tar.decodeBytes(bytes, verify: true);
+        expect(tar.files.length, equals(t['headers'].length));
+
+        for (var i = 0; i < tar.files.length; ++i) {
+          final file = tar.files[i];
+          var hdr = t['headers'][i] as Map<String, dynamic>;
+
+          if (hdr.containsKey('Name')) {
+            expect(file.filename, equals(hdr['Name']));
+          }
+          if (hdr.containsKey('Mode')) {
+            expect(file.mode, equals(hdr['Mode']));
+          }
+          if (hdr.containsKey('Uid')) {
+            expect(file.ownerId, equals(hdr['Uid']));
+          }
+          if (hdr.containsKey('Gid')) {
+            expect(file.groupId, equals(hdr['Gid']));
+          }
+          if (hdr.containsKey('Size')) {
+            expect(file.fileSize, equals(hdr['Size']));
+          }
+          if (hdr.containsKey('Linkname')) {
+            expect(file.nameOfLinkedFile, equals(hdr['Linkname']));
+          }
+          if (hdr.containsKey('ModTime')) {
+            expect(file.lastModTime, equals(hdr['ModTime']));
+          }
+          if (hdr.containsKey('Typeflag')) {
+            expect(file.typeFlag, equals(hdr['Typeflag']));
+          }
+          if (hdr.containsKey('Uname')) {
+            expect(file.ownerUserName, equals(hdr['Uname']));
+          }
+          if (hdr.containsKey('Gname')) {
+            expect(file.ownerGroupName, equals(hdr['Gname']));
+          }
+        }
+      });
+    }
+  });
 }
