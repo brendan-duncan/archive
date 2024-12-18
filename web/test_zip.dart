59a3843ffa94396aa0432b887eada5d37b08dd4f
Brendan Duncan <brendanduncan@gmail.com>
2024-12-18 09:27:41 -0700

    update web build test

--- web/test_zip.dart
+++ web/test_zip.dart
@@ -1,35 +1,26 @@
 // ignore_for_file: avoid_print
-
-import 'dart:html';
-import 'dart:typed_data';
-
 import 'package:archive/archive.dart';
+import 'package:http/http.dart' as http;
+import 'package:web/web.dart';
 
-void main() {
+void main() async {
   // An img on the html page is used to establish the path to the images
   // directory.  It's removed after we get the path since we'll be populating
   // the page with our own decoded images.
-  final img = querySelectorAll('img')[0] as ImageElement;
-  final path = img.src!.substring(0, img.src!.lastIndexOf('/'));
+  final img = document.querySelectorAll('img').item(0) as HTMLImageElement;
+  final path = img.src.substring(0, img.src.lastIndexOf('/'));
   img.remove();
 
   // Use an http request to get the image file from disk.
-  var req = HttpRequest();
-  req.open('GET', '$path/readme.zip');
-  req.responseType = 'arraybuffer';
-  req.onLoadEnd.listen((e) {
-    if (req.status == 200) {
-      // Convert the text to binary byte list.
-      final bytes = Uint8List.view(req.response as ByteBuffer);
-      print(bytes);
-      final archive = ZipDecoder().decodeBytes(bytes);
-      print('NUMBER OF FILES ${archive.length}');
-      print(archive[0].name);
-      print(archive[0].size);
-      print(archive[0].compression);
-      final decoded = archive[0].readBytes();
-      print(decoded);
-    }
-  });
-  req.send('');
+  var url = Uri.parse('$path/readme.zip');
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    final archive = ZipDecoder().decodeBytes(response.bodyBytes);
+    print('NUMBER OF FILES ${archive.length}');
+    print(archive[0].name);
+    print(archive[0].size);
+    print(archive[0].compression);
+    final decoded = archive[0].readBytes();
+    print(String.fromCharCodes(decoded!));
+  }
 }
