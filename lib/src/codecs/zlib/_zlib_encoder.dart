1fd37de636947851ee85fd747ae820bd9f59c87e
CodeDoctorDE <codedoctor@linwood.dev>
2024-12-18 22:07:04 +0100

    Fix js import

--- lib/src/codecs/zlib/_zlib_encoder.dart
+++ lib/src/codecs/zlib/_zlib_encoder.dart
@@ -1,3 +1,3 @@
 export '_zlib_encoder_stub.dart'
     if (dart.library.io) '_zlib_encoder_io.dart'
-    if (dart.library.js) '_zlib_encoder_web.dart';
+    if (dart.library.js_interop) '_zlib_encoder_web.dart';
