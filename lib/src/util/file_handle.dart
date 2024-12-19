fb948a8edd90f5710b011398d75eb691e7d75547
CodeDoctorDE <codedoctor@linwood.dev>
2024-12-19 18:08:05 +0100

    Fix missing file_handle conditional import change

--- lib/src/util/file_handle.dart
+++ lib/src/util/file_handle.dart
@@ -1,3 +1,1 @@
-export '_file_handle.dart'
-    if (dart.library.io) '_file_handle_io.dart'
-    if (dart.library.js_interop) '_file_handle_html.dart';
+export '_file_handle_html.dart' if (dart.library.io) '_file_handle_io.dart';
