f5282373b0ae8df03460df392189a83371b7607e
eu <ekuleshov@gmail.com>
2025-05-31 22:40:53 -0400

    removed Adler32 and Crc32 classes

--- test/adler32_test.dart
+++ test/adler32_test.dart
@@ -23,28 +23,4 @@ void main() {
       expect(adlerVal, 0x96C8DE2B);
     });
   });
-
-  // group('adler32 class', () {
-  //   test('empty', () {
-  //     final adler = Adler32();
-  //     expect(adler.close(), [0, 0, 0, 1]);
-  //   });
-  //   test('1 byte', () {
-  //     final adler = Adler32();
-  //     adler.add([1]);
-  //     expect(adler.close(), [0, 0x02, 0, 0x02]);
-  //   });
-  //   test('10 bytes', () {
-  //     final adler = Adler32();
-  //     adler.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
-  //     expect(adler.close(), [0, 0xDC, 0, 0x2E]);
-  //   });
-  //   test('100000 bytes', () {
-  //     final adler = Adler32();
-  //     for (var i = 0; i < 10000; i++) {
-  //       adler.add([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
-  //     }
-  //     expect(adler.close(), [0x96, 0xC8, 0xDE, 0x2B]);
-  //   });
-  // });
 }
